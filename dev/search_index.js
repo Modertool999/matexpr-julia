2a1cfefcf6d109b43a0afd3f85405c0a7eb42b64
Documenter.jl <documenter@juliadocs.github.io>
2026-03-31 19:06:20 +0000

    build based on a5c04ab

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Matexpr.jl-Documentation","page":"Home","title":"Matexpr.jl Documentation","text":"Welcome to the documentation for Matexpr.jl.","category":"section"},{"location":"#Function-Documentation","page":"Home","title":"Function Documentation","text":"```@docs filterlinenumbers(e)","category":"section"}]
+}
